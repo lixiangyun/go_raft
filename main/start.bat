94f660d8288b0b0b08a406aafc66e7cdb09af7cf
Panda <linimbus@126.com>
2017-08-15 07:48:17 +0800

    init project

---
+++ main/start.bat
@@ -0,0 +1,3 @@
+start init.bat localhost:8001 localhost:8002 localhost:8003
+start init.bat localhost:8002 localhost:8003 localhost:8001
+start init.bat localhost:8003 localhost:8002 localhost:8001 
