14ae65ed5797d6df7532dab6453a25d29bb4eee3
Panda <linimbus@126.com>
2017-08-17 23:10:52 +0800

    add test

--- main/main.go
+++ main/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"crypto/rand"
 	"go_raft/src"
 	"log"
 	"os"
 	"time"
 )
+
+func getRandInt() int {
+
+	var buf [100]byte
+	var val int
+
+	for i := 0; i < 100; i++ {
+		_, err := rand.Read(buf[0:])
+		if err != nil {
+			val += time.Now().Nanosecond()
+		} else {
+			val += int(buf[0]) + int(buf[1])<<8 + int(buf[2])<<16 + int(buf[3])<<24
+		}
+	}
+
+	return val
+}
 
 func main() {
 
@@ -29,9 +47,16 @@ func main() {
 
 	log.Println("Server start ok!")
 
-	for {
+	// for test
+	val := (getRandInt() % 100) + 10
+
+	log.Println("proccess maybe exit at ", val, " second laster.")
+
+	for i := 0; i < val; i++ {
+
 		// run forever not to stop
-		time.Sleep(time.Duration(10 * time.Second))
+		time.Sleep(time.Duration(time.Second))
+		log.Println("pass ", i, " second.")
 	}
 
 	log.Println("Server stop ok!")
